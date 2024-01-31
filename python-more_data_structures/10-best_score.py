b1166033ec65aa7fc952c3649a8925bcf1c4edd3
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-31 13:22:14 -0800

    Pycodestyle validation for best_score.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     try:
         values = sorted(a_dictionary.values())
         for key in a_dictionary.keys():
-             if a_dictionary[key] == values[-1]:
-                 return key
-    except:
+            if a_dictionary[key] == values[-1]:
+                return key
+    except Exception:
         return None
