b6b2bbfcded22c8092375da83dd902b214bb0db2
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-01 04:43:05 -0800

    1-square.py - Class Square with size private attribute.

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This file contains Square class."""
+
+
+class Square():
+    """Documentation for Class:
+    This class contains size attribute."""
+    def __init__(self, size):
+        """Initial function for Square class"""
+        self.__size = size
