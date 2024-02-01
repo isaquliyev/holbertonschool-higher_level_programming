6e328ba1c37019f7ce11258d0abda1a1f7ea237d
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-01 06:13:52 -0800

    6-square.py - Position created and my_print modified.

---
+++ python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""This file contains Square class inside."""
+
+
+class Square():
+    """Documentation for Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Init block of Square Class"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """size function that returns private size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Size function for set value for size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """position property that retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """sets value for position."""
+        if not isinstance(value, tuple) and not isinstance(value[0], int)\
+           and not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Area mothod that calculates area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints area using # sign"""
+        for i in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
