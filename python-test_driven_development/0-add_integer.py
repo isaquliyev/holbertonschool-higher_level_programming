c82f1f1cb8e767a75324afab1d7cf1daf82b5f44
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-03 09:21:22 -0800

    Pycodestyle Validation!

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     """
     Simple Algorithm that adds two integer and return sum
     """
-    if not(isinstance(a, int) or isinstance(a, float)):
+    if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if not(isinstance(b, int) or isinstance(b, float)):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
