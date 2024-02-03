76e3feecc517fac8a9e7ab55d720de3774d8e277
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-03 04:58:13 -0800

    101-square - my_print method substituted by __str__

---
+++ python-classes/101-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""This file contains Square class inside."""
+
+
+class Square():
+    """Documentation for Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Init block of Square Class"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """size function that returns private size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Size function for set value for size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """position property that retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """sets value for position."""
+        if not (isinstance(value, tuple)
+           and len(value) == 2
+           and isinstance(value[0], int)
+           and isinstance(value[1], int)
+           and value[0] >= 0
+           and value[1] >= 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Area mothod that calculates area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Prints area using # sign"""
+        res = ""
+        if self.__size == 0:
+            res += "\n"
+        else:
+            for i in range(self.position[1]):
+                res += "\n"
+
+            for i in range(self.size):
+                for j in range(self.position[0]):
+                    res += " "
+                for j in range(self.__size):
+                    res += "#"
+                res += "\n"
+        return res[:-1]
