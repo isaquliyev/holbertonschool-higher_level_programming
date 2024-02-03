1e92c0ce87aa1c1660fb165ee7a46f3ca5e6c3e0
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-03 08:46:00 -0800

    9 test implemented correctly.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    Simple Algorithm that adds two integer and return sum
+    """
     if not(isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not(isinstance(b, int) or isinstance(b, float)):
