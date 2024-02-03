c451c4328fe0643d9d7196bee90b40265e93fea7
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-03 05:56:05 -0800

    I returned wrong thing.

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,4 +13,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list.append(result)
-    return result
+    return new_list
