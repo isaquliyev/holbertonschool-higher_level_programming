e209fd968774527dede8a0797c5abd8646a6df0f
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-04 05:30:38 -0800

    Where your grandma stay ha, Checker?

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,13 +11,19 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    temp = text
-    text = text.replace(". ", ".\n")
-    text = text.replace(".", ".\n")
-    text = text.replace("? ", "?\n")
-    text = text.replace("?", "?\n")
-    text = text.replace(": ", ":\n")
-    text = text.replace(":", ":\n")
-    if text != temp:
-        text += "\n"
-    print(text, end="")
+    text_list = []
+    single_sentence = ""
+    for letter in text:
+        single_sentence += letter
+        if (letter == "." or
+            letter == "?" or
+            letter == ":"):
+               text_list.append(single_sentence)
+               single_sentence = ""
+    text_list.append(single_sentence)
+    edited_list = []
+    for txt in text_list:
+        txt = txt.strip()
+        edited_list.append(txt)
+    output = "\n\n".join(edited_list)
+    print(output, end="")
