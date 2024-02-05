6bbc342e03cd3c8b734ea8dc80a425d206f1f240
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-05 12:12:21 -0800

    Docs edited.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+
 """
     Modul Documentation
 """
 
 
 def lookup(obj):
-    """ 
-        Lookup - list of all attributes
-    """
-    return dir(obj)
+    """ Lookup - list of all attributes """
+    return (dir(obj))
