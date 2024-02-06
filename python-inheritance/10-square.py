895d41d1d47e4d6b10ad1fd5f9e0c0fc56992167
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-06 05:40:30 -0800

    Square class created that inherits from Rectangle class.

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    The ``Geometry`` module.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    # Square class that inherits from Rectangle
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
