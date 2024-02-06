755174b290228d17d02bdd2c0ef286fdcb8a0f1e
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-06 01:58:52 -0800

    is_kind_of_class function implemented.

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    The ``3. Same class or inherit from`` module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks is instance or not """
+    return isinstance(obj, a_class)
