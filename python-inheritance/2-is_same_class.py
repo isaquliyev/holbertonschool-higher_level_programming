2b6f2f7876adf06d76d89dae614b867e97cdd112
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-06 01:42:14 -0800

    is_same_class function implemented.

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    The ``2. Exact same object`` module
+"""
+
+
+def is_same_class(obj, a_class):
+    """ Checks the class is same or not. """
+    if type(obj) == a_class:
+        return True
+    return False
