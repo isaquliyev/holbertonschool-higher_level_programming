b4f0d724d49f19a15bf3119f5e65207eee77c35a
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-06 06:16:59 -0800

    Module Documented.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+"""
+    The ``0. Read file`` module
+"""
+
+
 def read_file(filename=""):
+    """
+        read_file - a function that reads a text file (UTF8)
+        and prints it to stdout
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
