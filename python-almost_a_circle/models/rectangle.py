0b8402b506fc76533daf75b74930bac56c6527fc
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-11 13:48:35 -0800

    __str__ implemented.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,13 @@ class Rectangle(Base):
             for _ in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """
+            Returns class in this format:
+            [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        result = "[Rectangle] ({}) {}/{} - {}/{}".\
+            format(self.id, self.__x, self.__y,
+                   self.__width, self.__height)
+        return result
