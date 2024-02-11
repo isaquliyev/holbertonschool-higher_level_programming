a23b6b496f29f8bd87013227e0a596361b9b72ae
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-11 15:11:11 -0800

    [] substitued with "[]"

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,11 +19,12 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
             Function that returns the JSON string representation
             of list_dictionaries
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
