d7500aa7d2896844141b7fd34e79760b8adf25ed
isaquliyev <isa.quluyev.wip@gmail.com>
2024-02-11 14:39:51 -0800

    self.__size substituted with self.width

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,18 +14,16 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """
             Set the side size of Square
         """
-        self.__size = value
         self.width = value
         self.height = value
 
