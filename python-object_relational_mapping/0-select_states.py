7124f3790f62873b199b0ad285737dad2403279b
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-21 01:37:16 +0400

    Now, code will not be executed when imported

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-
+"""
+    The `Get all states` module
+"""
 import MySQLdb
 import sqlalchemy
 import sys
 
-argv = sys.argv
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
+if __name__ == "__main__":
+    argv = sys.argv
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db, charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=user,
+                           passwd=passwd,
+                           db=db,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
