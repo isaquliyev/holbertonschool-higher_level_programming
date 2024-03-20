8ef1ee034bc231a485ed9b7308966e08f808732a
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-21 02:40:47 +0400

    3-my_safe_filter_states.py - SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    The `SQL Injection...` module
+"""
+import MySQLdb
+import sqlalchemy
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    name = argv[4]
+    name = str(name.split(chr(39))[0])
+    name = str(name.split(chr(34))[0])
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=user,
+                           passwd=passwd,
+                           db=db,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+                    SELECT * FROM states
+                    WHERE name = "Arizona"
+                    ORDER BY id ASC
+                """.format(name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    conn.close()
