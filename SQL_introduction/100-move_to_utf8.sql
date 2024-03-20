efead06d4176d53f4e16cad38c7096a2daec6add
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-20 13:06:55 +0000

    utf8mb4 format.

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,4 @@
 -- SQL Query  that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-USE hbtn_0c_0
-ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
