cac81d795c1018d6aa0033b79b904e988c4552e2
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-20 12:56:56 +0000

    Convert character set

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- SQL Query  that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE hbtn_0c_0
+ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
