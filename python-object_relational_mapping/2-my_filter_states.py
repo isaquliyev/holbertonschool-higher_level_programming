cd05e405377cde29f9350f1ddbab7689965cae4a
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-21 02:17:31 +0400

    2-my_filter_states.py - Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    The `Filter states by user input` module
+"""
+import MySQLdb
+import sqlalchemy
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    name = argv[4]
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=user,
+                           passwd=passwd,
+                           db=db,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+                    SELECT * FROM states
+                    WHERE name = "{}"
+                    ORDER BY id ASC
+                """.format(name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
