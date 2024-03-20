53e33027f036abf2c32db1320b213bd37fa3fd0f
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-21 02:23:26 +0400

    Condition added within if block

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
                 """.format(name))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     conn.close()
