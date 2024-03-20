1c6b7d36e10bf846b4ef803518237ceb372d88c3
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-20 14:15:25 +0000

    2-create_read_user.sql - Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- SQL Query that creates the databse hbtn_0d_2 MySQL server user user_0d_2.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
