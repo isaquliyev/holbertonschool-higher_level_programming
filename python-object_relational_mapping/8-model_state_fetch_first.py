2d7b7e916e193860ae039f390eaaeb181da9ebe2
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-21 17:29:36 +0400

    8-model_state_fetch_first.py - First state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    The `First state` module.
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, db),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).all()[0]
+    print("{}: {}".format(first_state.id, first_state.name))
+    session.close()
