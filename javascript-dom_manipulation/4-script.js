af6b9bfe60c1b17165f12fa40752ad7fd365e2e0
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-26 16:30:42 +0400

    What a solution :)

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,5 @@
+document.getElementById('add_item').onclick = addItem;
+
+function addItem () {
+    document.querySelector('ul.my_list').innerHTML = document.querySelector('ul.my_list').innerHTML + '<li>Item</li>';
+}
