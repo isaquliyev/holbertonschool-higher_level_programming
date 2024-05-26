6073afd96df139a521c10a0b6469db24ffff6b72
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-26 16:35:16 +0400

    Semistandard o7

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,5 +1,5 @@
 document.getElementById('add_item').onclick = addItem;
 
 function addItem () {
-    document.querySelector('ul.my_list').innerHTML = document.querySelector('ul.my_list').innerHTML + '<li>Item</li>';
+  document.querySelector('ul.my_list').innerHTML = document.querySelector('ul.my_list').innerHTML + '<li>Item</li>';
 }
